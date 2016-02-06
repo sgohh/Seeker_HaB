3f94a3122606ddf2f6d084548cc78224527db51e
Rachel Markell <rachelmarkell@Rachels-MacBook-Pro-3.local>
2016-02-06 14:59:40 -0500

    commiting profile

--- HackAtBrown/ViewController.swift
+++ HackAtBrown/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        image.layer.borderWidth = 1.0
+        image.layer.masksToBounds = false
+        image.layer.borderColor = UIColor.whiteColor().CGColor
+        image.layer.cornerRadius = 13
+        image.layer.cornerRadius = image.frame.size.height/2
+        image.clipsToBounds = true 
     }
 
     override func didReceiveMemoryWarning() {
